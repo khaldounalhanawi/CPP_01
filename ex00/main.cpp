d76310c68df1bbdea7a5735d78d9dd6963c1d5f3
Khaldoun Alhanawi <khaldounalhanawi@users.noreply.github.com>
2026-04-04 10:48:55 +0200

    ex00_added make file

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kalhanaw <kalhanaw@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/03 19:26:33 by kalhanaw          #+#    #+#             */
-/*   Updated: 2026/04/03 19:26:33 by kalhanaw         ###   ########.fr       */
+/*   Updated: 2026/04/03 23:59:51 by kalhanaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,7 @@ int	main(void)
 	Zombie *p;
 
 	randomChump ("EvilMouse");
-
 	p = newZombie ("Muffen");
 	delete (p);
-
-
 	return (0);
 }
